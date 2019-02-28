723e907c536b78331236e4e15809cea74c22c446
Derek <derekdenhartigh@gmail.com>
2019-02-28 14:43:49 -0500

    Finished the exercise, ways to improve: maybe have redundant items given a counter rather than be repeated, have list flow down rather than overflow over the form.

--- main.js
+++ main.js
@@ -2,23 +2,22 @@
 (function () {
     document.getElementById("submit-classification").addEventListener("click", ()=> {
 
-        let inputValue = document.getElementById("submit-classification").value;
-        let fruitList = document.getElementById("fruit-list");
+        let inputValue = document.getElementById("input-area").value;
         let vegetableList = document.getElementById("vegetable-list");
-        console.log(fruitList, vegetableList);  // for testing, targeting of these lists successful
+        let fruitList = document.getElementById("fruit-list");
+
         if (document.getElementById("fruit-button").checked === true){
             let newListItem = document.createElement("li");
-            newListItem.innerHTML="Banana"; // for testing, 
-            // newListItem.innerText(`${inputValue}`);
-            fruitList = document.getElementById("fruit-list");
-            document.fruitList.appendChild(newListItem);
+            newListItem.innerText = inputValue;
+            fruitList.appendChild(newListItem);
+            return;
         }
 
         if (document.getElementById("vegetable-button").checked === true){
             let newListItem = document.createElement("li");
-            newListItem.innerText(`${inputValue}`);
-            fruitList = document.getElementById("fruit-list");
-            document.vegetableList.appendChild(newListItem);
+            newListItem.innerText = inputValue;
+            vegetableList.appendChild(newListItem);
+            return;
         }
 
         else alert("Did you select a category?  Click one of them lil radio buttons and try again, or not, what do I care I'm just a program.\nI'm not even really talking to you right now, this is your own head voice conversing with itself.\n*Mind Blown*");
